b9231931d002a3f02c9e2eb7646f028ec9e19e07
Nirmala Mangrati <nirmalamgrt@gmail.com>
2026-08-07 12:31:46 +0545

    add admin login

--- cloudinary.js
+++ cloudinary.js
@@ -1,10 +1,8 @@
-import { v2 as cloudinary } from "cloudinary";
+import cloudinary from "cloudinary";
 
-// Cloudinary configuration
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
-
-export default cloudinary;
+export default cloudinary.v2;
--- middleware/verifyAuth.js
+++ middleware/verifyAuth.js
@@ -1,12 +1,11 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = "your_jwt_secret_key"; 
+const JWT_SECRET = "your_jwt_secret_key";
 
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader)
     return res.status(401).json({ message: "Authorization header missing" });
-
   const token = authHeader.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Token missing" });
 
