f1ade75eed738550a35db6aefdd3acf2588d0203
Nirmala Mangrati <nirmalamgrt@gmail.com>
2026-08-07 12:31:59 +0545

    add admin login

--- middleware/verifyAuth.js
+++ middleware/verifyAuth.js
@@ -8,7 +8,6 @@ export const verifyToken = (req, res, next) => {
     return res.status(401).json({ message: "Authorization header missing" });
   const token = authHeader.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Token missing" });
-
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded;
