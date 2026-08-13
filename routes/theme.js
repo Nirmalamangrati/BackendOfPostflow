3c025acf3345547a2219826594aeff93ae6b4f8c
Nirmala Mangrati <nirmalamgrt@gmail.com>
2026-08-13 07:46:40 +0545

    add admin login

--- routes/theme.js
+++ routes/theme.js
@@ -58,7 +58,6 @@ router.patch("/theme/:id", verifyToken, async (req, res) => {
     if (!post) return res.status(404).json({ message: "Post not found" });
     if (post.userId.toString() !== req.user.id)
       return res.status(403).json({ message: "Unauthorized" });
-
     post.caption = text || post.caption;
     await post.save();
 
